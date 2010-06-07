1699c492968429ff46ea46346001ff339393b20c
Jordi Chacón <jordi.chacon@gmail.com>
2010-06-07 10:01:17 +0200

    Changed the top menu so that we can add as many languages as we want. Now we have a dropdown menu that lists all the languages

--- src/polish_common.erl
+++ src/polish_common.erl
@@ -23,19 +23,26 @@ left() ->
 
 
 header(Selected) ->
-    wf:wire(l2a(Selected), #add_class { class=selected }),
-    #panel { class=menu, body=lang_links()++[
-	#literal { text = "    -    "},
-        #link { class=search,  url='#search_form',     text="Search"  },
-        #link { class=statsbutton,   url='#stats',           text="Status"  },
-        #link { id=write,  url='/?action=show_changes',text="Generate po file"  },
-        #link { id=write,  url='/logout',text="Logout"  }
+    #panel { class=menu, body=[
+        #list{body=[build_dropdown_menu(Selected),
+	#listitem{ body=[#link { text = "    -    "}]},
+        #listitem{ body=[#link { class=search,  url='#search_form',     text="Search"  }]},
+        #listitem{ body=[#link { class=statsbutton,   url='#stats',           text="Status"  }]},
+        #listitem{ body=[#link { id=write,  url='/?action=show_changes',text="Generate po file"  }]},
+        #listitem{ body=[#link { id=write,  url='/logout',text="Logout"  }]}]}
     ]}.
 
+build_dropdown_menu(Selected0) ->
+    Selected = case Selected0 of
+		   [] -> "Languages";
+		   _  -> Selected0
+	       end,
+    #listitem{body = [#link{text=Selected}, #list{body = lang_links()}]}.
+
 lang_links() ->
-    [#link { id=list_to_atom(string:to_lower(gettext_iso639:lc2lang(LC))),    
+    [#listitem { body = [#link{id=list_to_atom(string:to_lower(gettext_iso639:lc2lang(LC))),    
              url="/?po="++LC,        
-             text=gettext_iso639:lc2lang(LC)  }
+             text=gettext_iso639:lc2lang(LC)  }]}
      || LC <- all_custom_lcs()].
 
 
