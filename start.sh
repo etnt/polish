62fda1febb6bd83eef029dba246516e78ff70321
Jordi Chacón <jordi.chacon@klarna.com>
2010-09-10 09:11:32 +0200

    Improved start.sh script so that the developer starts polish now without editing the polish.app file. A new argument has been added to the start script instead: absolute path to the lang directory

--- start.sh
+++ start.sh
@@ -3,11 +3,18 @@ cd `dirname $0`
 
 . ./dep.inc
 
-echo "Starting Polish..."
-erl \
-    -sname ${NAME} \
-    -pa ./ebin ${NITROGEN_EBIN} ${SIMPLE_BRIDGE_EBIN} ${NPROCREG_EBIN} \
+if [ $# -eq 1 ]; then
+    hostname="\""$(hostname)"\""
+    po_lang_dir="\""$(echo $1 | sed 's/=.*//')"\""
+    echo "Starting Polish..."
+    erl \
+	-polish hostname $hostname po_lang_dir $po_lang_dir \
+	-sname ${NAME} \
+	-pa ./ebin ${NITROGEN_EBIN} ${SIMPLE_BRIDGE_EBIN} ${NPROCREG_EBIN} \
         ${GETTEXT_EBIN} ${EOPENID_EBIN} ${ELOGGER_EBIN} ${TRANE_EBIN} \
-    -eval "application:start(nprocreg)" \
-    -eval "application:start(polish)"
+	-eval "application:start(nprocreg)" \
+	-eval "application:start(polish)"
 
+else
+    echo "Usage: ./start.sh 'absolute_path_to_lang_dir'"
+fi
