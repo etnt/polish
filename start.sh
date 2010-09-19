ea3d4fc7f19e07a2f9ce4d4fc6a35ca1a914167c
Jordi Chacón <jordi.chacon@klarna.com>
2010-09-19 08:19:08 +0200

    start script adapted to the new ask_replace_keys env variable. this variable is set to true by default so before washing the po files, polish will ask the developer if he/she wants to replace a new key with an old one and after that the po files will be washed accordingly. If the dev do not want to replace any key, he/she can use the --no-replace-key flag to wash the po files directly

--- start.sh
+++ start.sh
@@ -3,17 +3,30 @@ cd `dirname $0`
 
 . ./dep.inc
 
+ask_replace_keys=true
+
 if [ $# -eq 1 ]; then
     po_lang_dir="\""$(echo $1 | sed 's/=.*//')"\""
-    echo "Starting Polish..."
-    erl \
-	-polish po_lang_dir $po_lang_dir \
-	-sname ${NAME} \
-	-pa ./ebin ${NITROGEN_EBIN} ${SIMPLE_BRIDGE_EBIN} ${NPROCREG_EBIN} \
-        ${GETTEXT_EBIN} ${EOPENID_EBIN} ${ELOGGER_EBIN} ${TRANE_EBIN} \
-	-eval "application:start(nprocreg)" \
-	-eval "application:start(polish)"
-
-else
-    echo "Usage: ./start.sh absolute_path_to_lang_dir"
+else 
+    if [ $# -eq 2 -a $1 = "--no-replace-keys" ]; then
+	po_lang_dir="\""$(echo $2 | sed 's/=.*//')"\""
+	ask_replace_keys=false
+    else
+	echo ""
+	echo "Usage: ./start.sh [options] absolute_path_to_lang_dir"
+	echo ""
+	echo "       Options:"
+	echo "          --no-replace-keys :  Do not replace keys and wash po files directly"
+	echo ""
+	exit
+    fi
 fi
+
+echo "Starting Polish..."
+erl \
+    -polish po_lang_dir $po_lang_dir ask_replace_keys $ask_replace_keys \
+    -sname ${NAME} \
+    -pa ./ebin ${NITROGEN_EBIN} ${SIMPLE_BRIDGE_EBIN} ${NPROCREG_EBIN} \
+    ${GETTEXT_EBIN} ${EOPENID_EBIN} ${ELOGGER_EBIN} ${TRANE_EBIN} \
+    -eval "application:start(nprocreg)" \
+    -eval "application:start(polish)"
