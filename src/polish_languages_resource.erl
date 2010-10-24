4907ea016f5e289580e82f220199e244a636036e
Jordi Chacon <jordi.chacon@klarna.com>
2010-10-24 08:22:54 +0200

    Throw bad_uri when a get request against /language/bu when bu is not an existent resource

--- src/polish_languages_resource.erl
+++ src/polish_languages_resource.erl
@@ -11,4 +11,8 @@ get_list() ->
      [LC || LC <- polish:all_custom_lcs(), LC =/= "a"].
 
 get(LC) ->
-    polish_po:get_stats(LC).
+    case polish_po:get_stats(LC) of
+	{0, 0} -> throw(bad_uri);
+	Stats  -> Stats
+    end.
+
