2e36786dd900076928c98d2b510904d09d8c9719
Jordi Chacon <jordi.chacon@klarna.com>
2010-12-20 15:27:52 +0100

    Moved languages functionality from polish_po to polish_languages_resource

--- src/polish_languages_resource.erl
+++ src/polish_languages_resource.erl
@@ -8,12 +8,38 @@
 
 -include("polish.hrl").
 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%  A P I
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 get_list() ->
   [LC || LC <- polish:all_custom_lcs(), LC =/= "a"].
 
 get(LC) ->
-  case polish_po:get_stats(LC) of
+  case get_language_stats(LC) of
     {0, 0} -> throw(bad_uri);
     Stats  -> Stats
   end.
 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%  I N T E R N A L   F U N C T I O N S
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+get_language_stats(undefined) -> {0, 0};
+get_language_stats(LC) ->
+  KVs = polish_server:read_po_file(LC),
+  Untrans = get_amount_untranslated_keys(KVs, ?l2a(LC)),
+  {length(KVs), Untrans}.
+
+get_amount_untranslated_keys(KVs, LCa) ->
+  lists:foldl(
+    fun({K, K}, Acc) ->
+	case polish_server:is_always_translated(
+	       polish_utils:generate_key_identifier(K, ?a2l(LCa))) of
+	  true  -> Acc;
+	  false -> Acc + 1
+	end;
+       ({_K, _V}, Acc) -> Acc
+    end, 0, KVs).
--- src/polish_po.erl
+++ src/polish_po.erl
@@ -4,36 +4,4 @@
 
 -module(polish_po).
 
--export([get_stats/1]).
-
--include("polish.hrl").
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%  A P I
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-get_stats(undefined) -> {0, 0};
-get_stats(LC) ->
-  KVs = polish_server:read_po_file(LC),
-  Untrans = get_amount_untranslated_keys(KVs, ?l2a(LC)),
-  {length(KVs), Untrans}.
-
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%  I N T E R N A L   F U N C T I O N S
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
-% get_stats
-%------------------------------------------------------------------------------
-get_amount_untranslated_keys(KVs, LCa) ->
-  lists:foldl(
-    fun({K, K}, Acc) ->
-	case polish_server:is_always_translated(
-	       polish_utils:generate_key_identifier(K, ?a2l(LCa))) of
-	  true  -> Acc;
-	  false -> Acc + 1
-	end;
-       ({_K, _V}, Acc) -> Acc
-    end, 0, KVs).
+-export([]).
