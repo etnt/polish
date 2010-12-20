20c9321a75a91cb8aa1f95507786fabc5059789f
Jordi Chacon <jordi.chacon@klarna.com>
2010-12-20 14:49:33 +0100

    Moved stuff from Makefile to start script. Now the tests are run through the start script. You can also specify to run a specific suite and testcase.

--- start.sh
+++ start.sh
@@ -5,10 +5,34 @@ cd `dirname $0`
 
 ask_replace_keys=true
 
-if [ $# -eq 1 ]; then
-    po_lang_dir="\""$(echo $1 | sed 's/=.*//')"\""
-else 
-    if [ $# -eq 2 -a $1 = "--no-replace-keys" ]; then
+if [ $# -ge 1 -a $1 = "test" ]; then
+    mkdir -p results
+    if [ $# -eq 1 ]; then
+	run_test -dir test/ -logdir results/ \
+	    -pa $(pwd)/ebin $(pwd)/${MOCHIWEB_EBIN} $(pwd)/${GETTEXT_EBIN} \
+	    $(pwd)/${EOPENID_EBIN} $(pwd)/${ELOGGER_EBIN}
+    elif [ $# -eq 2 ]; then
+	run_test -suite $2 -logdir results/ \
+	    -pa $(pwd)/ebin $(pwd)/${MOCHIWEB_EBIN} $(pwd)/${GETTEXT_EBIN} \
+	    $(pwd)/${EOPENID_EBIN} $(pwd)/${ELOGGER_EBIN}
+    elif [ $# -eq 3 ]; then
+	run_test -suite $2 -case $3 -logdir results/ \
+	    -pa $(pwd)/ebin $(pwd)/${MOCHIWEB_EBIN} $(pwd)/${GETTEXT_EBIN} \
+	    $(pwd)/${EOPENID_EBIN} $(pwd)/${ELOGGER_EBIN}
+    fi
+    mv test/*beam ebin/
+elif [ $# -eq 1 -a $1 = "coverage" ]; then
+    mkdir -p results
+    cp ebin/*beam src/
+    run_test -dir test/ -cover priv/coverspec -logdir results/ \
+	-pa $(pwd)/ebin $(pwd)/${MOCHIWEB_EBIN} $(pwd)/${GETTEXT_EBIN} \
+	$(pwd)/${EOPENID_EBIN} $(pwd)/${ELOGGER_EBIN}
+    mv test/*beam ebin/
+    rm src/*beam
+else
+    if [ $# -eq 1 ]; then
+	po_lang_dir="\""$(echo $1 | sed 's/=.*//')"\""
+    elif [ $# -eq 2 -a $1 = "--no-replace-keys" ]; then
 	po_lang_dir="\""$(echo $2 | sed 's/=.*//')"\""
 	ask_replace_keys=false
     else
@@ -16,15 +40,22 @@ else
 	echo "Usage: ./start.sh [options] absolute_path_to_lang_dir"
 	echo ""
 	echo "       Options:"
-	echo "          --no-replace-keys :  Do not replace keys and wash po files directly"
+	echo "          --no-replace-keys                     :  Do not replace keys and wash po files directly"
+	echo ""
+	echo ""
+	echo "Test usage:"
+	echo "       ./start.sh test                          :  Run all testcases"
+	echo "       ./start.sh coverage                      :  Run all testcases with coverage"
+	echo "       ./start.sh test suite_name               :  Run all testcases in suite"
+	echo "       ./start.sh test suite_name testcase_name : Run testcase in suite"
 	echo ""
 	exit
     fi
+    echo "Starting Polish..."
+    erl \
+	-polish po_lang_dir $po_lang_dir ask_replace_keys $ask_replace_keys \
+	-sname ${NAME} \
+	-pa ./ebin ${MOCHIWEB_EBIN} ${GETTEXT_EBIN} ${EOPENID_EBIN} ${ELOGGER_EBIN} \
+	-eval "application:start(polish)"
 fi
 
-echo "Starting Polish..."
-erl \
-    -polish po_lang_dir $po_lang_dir ask_replace_keys $ask_replace_keys \
-    -sname ${NAME} \
-    -pa ./ebin ${MOCHIWEB_EBIN} ${GETTEXT_EBIN} ${EOPENID_EBIN} ${ELOGGER_EBIN} \
-    -eval "application:start(polish)"
